06f056e4aab57999e6ca31c56f39b219d29e8cab
Guillermo Cosio <1729882@TCS.com>
2019-09-24 16:51:07 -0300

    2.1

--- src/main/java/com/cosioguille/repository/UserRepository.java
+++ src/main/java/com/cosioguille/repository/UserRepository.java
@@ -10,8 +10,8 @@ import com.cosioguille.model.User;
 @Repository("userRepository")
 public interface UserRepository extends JpaRepository<User, Integer> {
 	
-	@Query("select case when count(*) > 0 then \"true\" else \"false\" end as boolean "
-			+ "from users where username = :username and password = :password")
-    public Boolean authenticate(@Param("username") String username, @Param("password")String password);
+	@Query("select case when count(*) > 0 then 'true' else 'false' end as boolean "
+			+ "from users u where u.username = :username and u.password = :password")
+    Boolean authenticate(@Param("username") String username, @Param("password")String password);
 
 }
